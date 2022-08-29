981633d4a6ce0158cc6daecf62a10fc4dde9c808
yuko0627 <jupos1123@gmail.com>
2022-08-29 19:24:59 +0900

    マイグレーションファイル作成

--- src/app/Http/Controllers/RegisterController.php
+++ src/app/Http/Controllers/RegisterController.php
@@ -7,6 +7,15 @@ use Illuminate\Http\Request;
 class RegisterController extends Controller
 {
     public function index(){
-        //会員登録画面を表示する処理
+        //会員登録画面を表示
+        return view('register');
+    }
+    public function store(Request $request){
+        $request->validate([
+            'name'=>'required',
+            'password'=>'required',
+            'instrument'=>'required'
+        ]);
+        
     }
 }
---
+++ src/resources/views/register.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ユーザー登録</title>
+</head>
+<body>
+    <form action="" method="post">
+        <input type="text" name="name">
+        <input type="password" name="password">
+        <input type="text" name="instrument">
+        <input type="submit" value="登録する">
+    </form>
+</body>
+</html>
