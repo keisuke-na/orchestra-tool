af0db26b07d715c6ec5d3e6361c0a3a160371662
yuko0627 <jupos1123@gmail.com>
2022-08-29 18:07:45 +0900

    会員登録画面を表示する処理

---
+++ src/app/Http/Controllers/RegisterController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    public function index(){
+        //会員登録画面を表示する処理
+    }
+}
